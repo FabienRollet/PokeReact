5daa57a5a8cfbeb3e3e70c792fd5705f45442775
Rollet Fabien <fabrol47@gmail.com>
2024-03-26 14:57:02 +0100

    Added 2de pokemon display

--- src/componants/PokemonCard.jsx
+++ src/componants/PokemonCard.jsx
@@ -1,9 +1,24 @@
 function PokemonCard () {
+    let pokemon=pokemonList[1]
     return(
         <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="♥ Best pokemon Bulbasaur ♥" />
-        <figcaption>♥ Best Pokemon Bulbasaur ♥</figcaption>
+             {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc} alt={pokemon.name} />
+            ) : (
+                <img src ="https://media1.tenor.com/m/xLU0RdmhhOMAAAAC/pas-de-panneau-ya-pas-de-panneau.gif"/>
+            )}
         </figure>
     );
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 export default PokemonCard;
