3fe1c139ad88c21bb8e04694c2566d239658ba27
Rollet Fabien <fabrol47@gmail.com>
2024-03-26 16:18:52 +0100

    first props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
 import './App.css'
 import PokemonCard from './componants/PokemonCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
   return (
     <>
-<PokemonCard></PokemonCard>
+<PokemonCard pokemon ={pokemonList[0]}/>
     </>
   )
 }
